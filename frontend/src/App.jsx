2203f401f627d191e40ca74ddd3914a1393be868
soumyalathaK <kalagurilatha@gmail.com>
2026-08-07 17:00:31 +0530

    Enhance Technology and Contact page UI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,20 @@
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+// import Home from "./pages/Home";
+// import Login from "./pages/Login";
+// import AdminDashboard from "./pages/admin/AdminDashboard";
+
+// export default function App() {
+//   return (
+//     <BrowserRouter>
+//       <Routes>
+//         <Route path="/" element={<Home />} />
+//         <Route path="/login" element={<Login />} />
+//         <Route path="/admin" element={<AdminDashboard />} />
+//       </Routes>
+//     </BrowserRouter>
+//   );
+// }
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Home from "./pages/Home";
